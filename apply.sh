3cc23a20a24551b4bafa732f726e34cabcd114f4
steadfasterX <steadfasterX@users.noreply.github.com>
2023-04-03 09:49:45 +0200

    import patch apply script

---
+++ apply.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+for p in $(find external/patches -type f -name '*.diff' -exec grep -H project {} \; | tr ' ' '#'); do
+    P=$(echo "$p" | sed 's#^#-i '$(pwd)'/#g;s/:project#/ -d /g')
+    echo -e "\n******************************************\nPATCH FILE DETECTED\napplying: ${p/*#}...\n\n"
+    repo forall "${p/*#}" -c 'git reset --hard' && echo "git reset hard finished for $p"
+    patch -r - --no-backup-if-mismatch --forward --ignore-whitespace --verbose -p1 $P
+    [ $? -eq 2 ] && echo "ERROR occured!!!" && exit 3
+    echo -e "******************************************\n"
+done
